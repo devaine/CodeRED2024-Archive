01e16e8b6b91d81fdc148faca2cc244b7ee940ae
Lance <lancefinke@icloud.com>
2024-02-10 22:33:03 -0600

    Filtering has been implemented

--- Retrive_Data.py
+++ Retrive_Data.py
@@ -1,17 +1,36 @@
 # Install the Python library from https://pypi.org/project/amadeus
-from amadeus import Client, ResponseError
+from amadeus import *
 
 import requests
 import json
 
-token = 'tM2fsmUbRliFICATKxVuQgM9aowX'
-headers = {'Authorization': 'Bearer ' + token}
+amadeus = Client(
+    client_id = "VW4WHbtHi5FKIJ87hRUjVFoRQ9OkD3RA",
+    client_secret = "11hTTsTumxjbEyyS",
+    log_level = "warn"
+)
 
-resp = requests.get('https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=MAD&departureDate=2024-07-10&returnDate=2024-07-14&adults=1&nonStop=false&max=250', headers=headers)
 
-offers = resp.json()["data"]
 
-print(offers)
+response = amadeus.shopping.flight_offers_search.get(
+    # First four are required for the API
+    originLocationCode='MAD',
+    destinationLocationCode='ATH',
+    departureDate='2024-07-26',
+    adults = 2
+    
+)
+resp = json.dumps(response.data, indent=4)
+resp = json.loads(resp)
 
-prices = list(map(lambda x: float(x["price"]["grandTotal"]),offers))
+'''Data to retrive: , destination, departure and return dates,'''
+
+prices = list(map(lambda x: float(x["price"]["grandTotal"]),resp))
+cabins = list(map(lambda x: (x["travelerPricings"][0]["fareDetailsBySegment"][0]["cabin"]),resp))
+currency = list(map(lambda x: (x["price"]["currency"]),resp))
+departures = list(map(lambda x: (x["itineraries"][0]["segments"][0]["departure"]["iataCode"]),resp))
+
 print(prices)
+print(cabins)
+print(currency)
+print(departures)
